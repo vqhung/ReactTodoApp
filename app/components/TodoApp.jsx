8105b7c8285b17718784dbb151572367c815b189
Hung Vo <vqhung@gmail.com>
2016-12-27 20:44:04 +0700

    Add complete and created timestamp

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var uuid = require('node-uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -13,7 +14,8 @@ var TodoApp = React.createClass({
       showCompleted: false,
       searchText: '',
       todos: TodoAPI.getTodos()
-    }
+
+    };
   },
   componentDidUpdate: function () {
     TodoAPI.setTodos(this.state.todos);
@@ -24,7 +26,9 @@ var TodoApp = React.createClass({
       {
         id: uuid(),
         text: text,
-        completed: false
+        completed: false,
+        createdAt: moment().unix(),
+        completedAt: undefined
       }]
     });
   },
@@ -32,6 +36,7 @@ var TodoApp = React.createClass({
     var updatedTodos = this.state.todos.map((todo) => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
       return todo;
     });
--- app/tests/components/todoapp.test.jsx
+++ app/tests/components/todoapp.test.jsx
@@ -21,13 +21,17 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
   it('should update completed status on handleToggle',() => {
     var todoData =
     { id: 11,
       text: 'Walk the dog',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
 
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
@@ -38,5 +42,26 @@ describe('TodoApp', () => {
     todoApp.handleToggle(todoApp.state.todos[0].id);
 
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
+
+  it('should toggle todo from completed to incomplete',() => {
+    var todoData =
+    { id: 11,
+      text: 'Walk the dog',
+      completed: true,
+      createdAt: 0,
+      completedAt: 123
+    };
+
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(true);
+
+    todoApp.handleToggle(todoApp.state.todos[0].id);
+
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   });
 });
