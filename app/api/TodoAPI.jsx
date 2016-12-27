587c69289f678fe8b1b84e69272c461276653cda
Hung Vo <vqhung@gmail.com>
2016-12-27 15:39:24 +0700

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,30 @@ module.exports = {
     };
 
     return $.isArray(todos) ? todos : [] ;
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var todoText = todo.text.toLowerCase();
+      return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+    });
+
+    filteredTodos.sort((a,b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+
+    return filteredTodos;
   }
 };
